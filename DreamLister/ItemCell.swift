99c47c6853b5dbaed5fa59d45f098b384655566e
ThunderKO <Andy@AndyKo.local>
2017-05-05 15:07:40 +0800

    NSFetchedResultsControllerDelegate Update
    +attemptFetch(fetchRequest, dataSort, controller)
    +func willchange, didchange -> begin, end updates()
    +Created controller(FetchResultControllerObject)
    -switch case
    -used controller section for two functions
    -primary configureCell in ItemCell
    -secondary configureCell in MainVC
    -Set heightForRow = 150
    -UIUpdate(No sep. , No Scroll Indicator)
    -Generated three data for testing
    -testing success

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,25 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by KO TING on 5/5/2017.
+//  Copyright © 2017年 EdUHK. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    func configureCell(item: Item) {
+        
+        title.text = item.title
+        price.text = "$\(item.price)"
+        details.text = item.details
+        
+    }
+}
